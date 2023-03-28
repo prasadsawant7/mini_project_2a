5cf5f2135a3d60cb1dbd8aa77878ddde43103b0c
unknown <prasadsawant7>
2023-03-28 11:06:38 +0530

    Added Navbar

--- src/client/components/Navbar/Navbar.jsx
+++ src/client/components/Navbar/Navbar.jsx
@@ -1,9 +1,37 @@
+import Search from "../Sidebar/Search";
+import { BiMoon, BiBell, BiChevronDown } from "react-icons/bi";
 import "./Navbar.css";
 
 export default function () {
   return (
     <div className="navbar">
-      <h1>This is navbar</h1>
+      <div className="page-details">
+        <div className="page-name">Configurations</div>
+        <div className="bread-crumbs">
+          <div className="crumb">Dashboard</div>
+          &gt;
+          <div className="crumb">Configurations</div>
+        </div>
+      </div>
+      <Search />
+      <div className="theme">
+        <BiMoon />
+      </div>
+      <div className="notification">
+        <BiBell />
+      </div>
+      <div className="account-details">
+        <div className="profile-image">
+          <img src="../../../../public/assets/icons/dark_logo.ico" alt="Profile Image" />
+        </div>
+        <div className="profile-data">
+          <div className="profile-name">John Doe</div>
+          <div className="profile-email">johndoe2003@mail.com</div>
+        </div>
+        <div className="profile-expand">
+          <BiChevronDown />
+        </div>
+      </div>
     </div>
   );
 }
--- src/client/components/Sidebar/Sidebar.jsx
+++ src/client/components/Sidebar/Sidebar.jsx
@@ -5,16 +5,25 @@ import MainMenu from "./MainMenu";
 import Settings from "./Settings";
 import HelpCenter from "./HelpCenter";
 import Logout from "./Logout";
+import Navbar from "../Navbar/Navbar";
 
 export default function Sidebar() {
   return (
-    <div className="sidebar">
-      <Head />
-      <Search />
-      <MainMenu />
-      <Settings />
-      <HelpCenter />
-      <Logout />
-    </div>
+    <>
+      <div className="sidebar">
+        <Head />
+        <Search />
+        <MainMenu />
+        <Settings />
+        <HelpCenter />
+        <Logout />
+      </div>
+      <div className="main-content">
+        <Navbar />
+        <div className="content">
+          Content
+        </div>
+      </div>
+    </>
   );
 }
